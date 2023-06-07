dbc111c99d389b8b8d05cf508236d154c27f7c44
troh <dhxofla91@gmail.com>
2023-06-07 22:19:19 +0900

    Initial commit

---
+++ src/main/java/com/kakaostyle/documentapproval/DocumentApprovalApplication.java
@@ -0,0 +1,13 @@
+package com.kakaostyle.documentapproval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentApprovalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentApprovalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kakaostyle/documentapproval/DocumentApprovalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaostyle.documentapproval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentApprovalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
